8eca518b661a48eda8f06713b4828a809bc2c09a
delayedaa <delayedaa@gmail.com>
2020-10-23 11:06:07 -0500

    [clock.js] Add minute and hour conversion functions

--- clock.js
+++ clock.js
@@ -1,4 +1,16 @@
 function secondRotation(seconds) {
     let degRotation = seconds * 6
     return degRotation
+}
 
+function minuteRotation(seconds) {
+  let minutes = seconds * 60;
+  let degRotation = minutes * 6;
+  return degRotation;
+}
+
+function hourRotation(seconds) {
+  let hours = seconds * 3600;
+  let degRotation = hours * 30;
+  return degRotation;
+}
