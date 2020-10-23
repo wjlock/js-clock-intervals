53bfb0070dd40c3348d4a267efd42c1bb2a84fbd
Will Locklier <will.locklier@gmail.com>
2020-10-23 12:13:14 -0400

    [clock] Add tick function

--- clock.js
+++ clock.js
@@ -1,16 +1,24 @@
-function secondRotation(seconds) {
-    let degRotation = seconds * 6
-    return degRotation
-}
 
-function minuteRotation(seconds) {
-  let minutes = seconds * 60;
-  let degRotation = minutes * 6;
-  return degRotation;
-}
+document.addEventListener("DOMContentLoaded", function() {
+  function secondRotation(seconds) {
+      let degRotation = seconds * 6
+      return degRotation
+  }
 
-function hourRotation(seconds) {
-  let hours = seconds * 3600;
-  let degRotation = hours * 30;
-  return degRotation;
-}
+  function minuteRotation(seconds) {
+    let minutes = seconds * 60;
+    let degRotation = minutes * 6;
+    return degRotation;
+  }
+
+  function hourRotation(seconds) {
+    let hours = seconds * 3600;
+    let degRotation = hours * 30;
+    return degRotation;
+  }
+
+  function tick() {
+    console.log('tick');
+  }
+  setInterval(tick, 1000)
+})
